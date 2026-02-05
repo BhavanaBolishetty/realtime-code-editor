1db495d483cf944376bc602045292f1fee2af1d6
BhavanaBolishetty <bhavanabolishetty3@gmail.com>
2026-02-05 05:11:11 -0600

    Initial commit

---
+++ REALTIME CODE/backend/index.mjs
@@ -0,0 +1,129 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import path from "path";
+
+const app = express();
+app.use(express.json());
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const rooms = new Map();
+
+io.on("connection", (socket) => {
+  console.log("User Connected", socket.id);
+
+  let currentRoom = null;
+  let currentUser = null;
+
+  socket.on("join", ({ roomId, userName }) => {
+    if (currentRoom) {
+      socket.leave(currentRoom);
+      rooms.get(currentRoom).delete(currentUser);
+      io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom)));
+    }
+
+    currentRoom = roomId;
+    currentUser = userName;
+
+    socket.join(roomId);
+
+    if (!rooms.has(roomId)) {
+      rooms.set(roomId, new Set());
+    }
+
+    rooms.get(roomId).add(userName);
+
+    io.to(roomId).emit("userJoined", Array.from(rooms.get(currentRoom)));
+  });
+
+  socket.on("codeChange", ({ roomId, code }) => {
+    socket.to(roomId).emit("codeUpdate", code);
+  });
+
+  socket.on("leaveRoom", () => {
+    if (currentRoom && currentUser) {
+      rooms.get(currentRoom).delete(currentUser);
+      io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom)));
+
+      socket.leave(currentRoom);
+
+      currentRoom = null;
+      currentUser = null;
+    }
+  });
+
+  socket.on("typing", ({ roomId, userName }) => {
+    socket.to(roomId).emit("userTyping", userName);
+  });
+
+  socket.on("languageChange", ({ roomId, language }) => {
+    io.to(roomId).emit("languageUpdate", language);
+  });
+
+  socket.on("disconnect", () => {
+    if (currentRoom && currentUser) {
+      rooms.get(currentRoom).delete(currentUser);
+      io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom)));
+    }
+    console.log("user Disconnected");
+  });
+});
+
+app.post("/execute", (req, res) => {
+  const { language, code } = req.body;
+  const jobId = uuid();
+
+  if (language === "javascript") { //default
+    const filePath = `./backend/temp/${jobId}.js`;
+    fs.writeFileSync(filePath, code);
+
+    exec(`node ${filePath}`, (err, stdout, stderr) => {
+      fs.unlinkSync(filePath);
+      if (err) return res.json({ output: stderr });
+      res.json({ output: stdout });
+    });
+  } 
+  else if (language === "python") {
+    const filePath = `./backend/temp/${jobId}.py`;
+    fs.writeFileSync(filePath, code);
+
+    exec(`python ${filePath}`, (err, stdout, stderr) => {
+      fs.unlinkSync(filePath);
+      if (err) return res.json({ output: stderr });
+      res.json({ output: stdout });
+    });
+  } 
+  else {
+    res.json({ output: "Language not supported yet" });
+  }
+});
+
+
+
+const port = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+app.use(
+  express.static(
+    path.join(__dirname, "frontend", "code-editor", "dist")
+  )
+);
+
+app.use((req, res) => {
+  res.sendFile(
+    path.join(__dirname, "frontend", "code-editor", "dist", "index.html")
+  );
+});
+
+server.listen(port, () => {
+  console.log("server is working on port 5000");
+});
